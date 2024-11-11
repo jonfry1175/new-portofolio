1d99e39143dd0e8be75afe1e1881f5b7f4f7b562
jonfry1175 <odiboen@gmail.com>
2024-11-11 23:16:13 +0700

    feat: add link to the icon navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,9 +8,15 @@ const NavBar = () => {
                 <img className='mx-1 w-28 h-15' src={logo} alt="logo" />
             </div>
             <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-                <FaLinkedin />
-                <FaGithub />
-                <FaDiscord />
+                <a href="https://linkedin.com/in/jonfry-agung-marbun" target="_blank" >
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/jonfry1175" target="_blank" >
+                    <FaGithub />
+                </a>
+                <a href="https://discord.com/users/1043162437467258920" target='_blank'>
+                    <FaDiscord />
+                </a>
             </div>
         </nav>
     )
